e82543088225cb8fb62c48eac39fee29aa67531d
Md. Fazle Rabbi <fazle.rabbi@bracits.com>
2023-06-06 15:37:12 +0600

    working from post man

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -67,7 +67,7 @@ class LoginController extends Controller
             );
             $result->access_token=$access_token;
       
-            return ['code' => 1, 'data' => $result, 'msg' => 'user information updated'];
+            return ['code' => 0, 'data' => $result, 'msg' => 'user information updated'];
         }
        }catch(Exception $e){
         return ['code' => 1, 'data' => "No data found", 'msg' => (string)$e];
